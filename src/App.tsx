bfe591634b730bdaf6f570e8c7e2484bf1c93831
ankitsensei <main.ankitbhagat@gmail.com>
2024-11-09 19:47:06 +0530

    quality-service page made for desktop

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,8 @@ const App = () => {
   return (
     <div className="w-full h-full px-2 py-2 text-white bg-gradient-to-r from-neutral-950 from-10% to-purple-900 to-90% lg:px-20">
       <Navbar/>
-      <div className="lg:h-screen">
-        <Hero/>
-        <Boxes/>
-      </div>
+      <Hero/>
+      <Boxes/>
       <QualityServices/>
       <RecentWorks/>
       <Skills/>
--- src/components/QualityServices.tsx
+++ src/components/QualityServices.tsx
@@ -1,19 +1,19 @@
 
 const QualityServices = () => {
     return (
-        <div>
-            <div className="mt-20 flex flex-col items-center">
+        <div className="lg:flex lg:flex-col lg:items-center">
+            <div className="mt-20 flex flex-col items-center lg:pb-10">
                 <h1 className="font-bold text-5xl bg-gradient-to-r from-pink-600 via-purple-600 to-blue-400 bg-clip-text text-transparent">Quality Services</h1>
-                <p className="flex items-center justify-center text-sm w-full px-10 text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae vero repellendus explicabo, amet blanditiis maxime error veniam corrupti labore, quibusdam, sit natus.</p>
+                <p className="flex items-center justify-center text-sm w-full px-10 text-center lg:mt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae vero repellendus explicabo, amet blanditiis maxime error veniam corrupti labore, quibusdam, sit natus.</p>
             </div>
-            <div className="flex flex-col gap-2 mt-4">
-                <div className="w-full h-16 flex gap-10 items-center justify-between px-10 rounded-lg bg-gradient-to-r from-purple-600 to-violet-950">
+            <div className="flex flex-col gap-2 mt-4 lg:w-1/2 lg:">
+                <div className="w-full h-16 flex gap-10 items-center justify-between px-10 rounded-lg bg-gradient-to-r from-purple-600 to-violet-950 ">
                     <h1 className="font-bold text-xl w-40">Editing</h1>
-                    <p className="w-80">I edit videos for Weddings, YouTube, Instagram, etc. </p>
+                    <p className="w-80 lg:w-full">I edit videos for Weddings, YouTube, Instagram, etc. </p>
                 </div>
-                <div className="w-full h-16 flex gap-10 items-center justify-between px-10 rounded-lg bg-gradient-to-r from-purple-600 to-violet-950">
+                <div className="w-full h-16 flex gap-10 items-center justify-between px-10 rounded-lg bg-gradient-to-r from-purple-600 to-violet-950 ">
                     <h1 className="font-bold text-xl w-40">Website</h1>
-                    <p className="w-80">I use mostly use React Js for building websites.</p>
+                    <p className="w-80 lg:w-full">I use mostly use React Js for building websites.</p>
                 </div>  
             </div>
         </div>
