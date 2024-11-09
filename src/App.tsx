a728638b0a9069981e6a5020cf2cf04f0ff8316c
ankitsensei <main.ankitbhagat@gmail.com>
2024-11-09 13:12:59 +0530

    added context in boxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
-// import Boxes from "./components/Boxes"
+import Boxes from "./components/Boxes"
 const App = () => {
   return (
     <div className="w-full h-screen px-2 py-2 text-white bg-gradient-to-r from-neutral-950 from-10% to-purple-900 to-90%">
       <Navbar/>
       <Hero/>
-      {/* <Boxes/> */}
+      <Boxes/>
     </div>
   )
 }
