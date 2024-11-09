fbe551ab9aeb88ce5f04caf2c1ca57c8b02db835
ankitsensei <main.ankitbhagat@gmail.com>
2024-11-09 19:25:30 +0530

    Boxes for desktop

--- src/components/Boxes.tsx
+++ src/components/Boxes.tsx
@@ -1,7 +1,7 @@
 
 const Boxes = () => {
     return (
-        <div className="flex flex-wrap justify-center items-center mt-10 gap-4">
+        <div className="flex flex-wrap justify-center items-center mt-10 gap-4 lg:mt-20">
             <div className="flex flex-col items-center justify-center w-40 h-20 rounded-lg bg-gradient-to-r from-purple-600 to-zinc-900 bg-opacity-0">
                 <h1 className="text-purple-300 text-5xl">2+</h1>
                 <p className="text-sm">Years Editing Exp.</p> 
