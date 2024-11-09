d70b0db26b635561035ddf10eb720dcc6af77a2e
ankitsensei <main.ankitbhagat@gmail.com>
2024-11-09 20:13:42 +0530

    skills section styled for desktop

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,10 +2,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHtml5, faCss3, faJs, faReact, } from '@fortawesome/free-brands-svg-icons'
 const Skills = () => {
     return (
-        <div className="flex flex-col items-center mt-20">
+        <div className="flex flex-col items-center my-20">
             <h1 className="font-bold text-5xl bg-gradient-to-r from-pink-600 via-purple-600 to-blue-400 bg-clip-text text-transparent">Skills</h1>
             <p className="text-sm w-80 text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem sint doloremque consequuntur.</p>
-            <div className='mt-5 w-80 flex flex-wrap gap-2 '>
+            <div className='mt-5 w-80 flex flex-wrap gap-2 lg:mx-40 lg:w-[700px] lg:items-center lg:justify-center'>
                 <div className='text-center '>
                     <div className='w-24 h-24 bg-white bg-opacity-5 flex flex-col items-center justify-center rounded-lg gap-1'>
                         <FontAwesomeIcon icon={faHtml5} className='text-4xl text-red-600'/>
