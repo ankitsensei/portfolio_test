b4a6a0c9731faa58e36dd399c9f337c13e3ba28f
ankitsensei <main.ankitbhagat@gmail.com>
2024-11-09 19:13:41 +0530

    navbar-desktop

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import Boxes from "./components/Boxes"
 import QualityServices from "./components/QualityServices"
 import RecentWorks from "./components/RecentWorks"
 import Skills from "./components/Skills"
-const App = () => {
+const App = () => {  
   return (
-    <div className="w-full h-full px-2 py-2 text-white bg-gradient-to-r from-neutral-950 from-10% to-purple-900 to-90%">
+    <div className="w-full h-full px-2 py-2 text-white bg-gradient-to-r from-neutral-950 from-10% to-purple-900 to-90% lg:px-20">
       <Navbar/>
       <Hero/>
       <Boxes/>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,24 +3,24 @@ import Logo from '../assets/ben10.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars} from '@fortawesome/free-solid-svg-icons'
 const Navbar = () => {
-    const [clicked, setClicked] = useState(false); ;
+    const [clicked, setClicked] = useState(true); ;
     return (
-        <div className='fixed top-0 left-0 w-full p-2 z-50 bg-gradient-to-r from-[rgba(23,23,23,0.5)] to-[rgba(128,0,128,0.0)]'>
+        <div className='fixed top-0 left-0 w-full p-2 z-50 bg-gradient-to-r from-[rgba(23,23,23,0.5)] to-[rgba(128,0,128,0.0)] lg:px-20'>
             <div className='flex items-center justify-between'>
             <div className='flex gap-3 items-center'>
                 <img src={Logo} alt="Logo" className='rounded-full w-12'/>
                 <h1 className='font-bold'>Ankit Bhagat</h1>
             </div>
             <div className='px-5'>
-                <FontAwesomeIcon icon={faBars} onClick={() => setClicked(!clicked)}/>
+                <FontAwesomeIcon icon={faBars} onClick={() => setClicked(!clicked)} className='lg:text-2xl'/>
                 {       
                     clicked ? 
-                    <ul className='flex flex-col w-32 py-2 rounded-lg bg-gradient-to-r from-purple-600 to-zinc-900 justify-center items-start px-3 absolute top-16 right-0 gap-2 font-semibold z-40'>
-                        <li>Home</li>
-                        <li>Services</li>
-                        <li>Recent-Works</li>
-                        <li>Skills</li>
-                        <button className='w-20 h-6 flex items-center justify-center bg-gradient-to-r from-purple-600 to-zinc-900 rounded-full '>Hire Me</button>
+                    <ul className='flex flex-col w-32 py-2 rounded-lg bg-gradient-to-r from-purple-600 to-zinc-900 justify-center items-start px-3 absolute top-16 right-0 gap-2 font-semibold z-40 lg:h-64 lg:w-60 lg:text-2xl lg:gap-3 '>
+                        <li className='hover:text-purple-500 duration-300'>Home</li>
+                        <li className='hover:text-purple-500 duration-300'>Services</li>
+                        <li className='hover:text-purple-500 duration-300'>Recent-Works</li>
+                        <li className='hover:text-purple-500 duration-300'>Skills</li> 
+                        <button className='w-20 h-6 flex items-center justify-center bg-gradient-to-r from-purple-600 to-zinc-900 rounded-full lg:w-40 lg:h-10'>Hire Me</button>
                     </ul>: null
                 }
             </div>
